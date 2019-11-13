f4da297bfc56f3f48d7247f81b24fa9853994ec5
Sioni Summers <sioni.summers10@imperial.ac.uk>
2019-11-13 10:58:32 +0100

    Add post-float training for BNN

--- models/models.py
+++ models/models.py
@@ -184,19 +184,19 @@ def three_layer_model_binary_relu_double(Inputs, nclasses, l1Reg=0):
 
     model = Sequential()
 
-    model.add(BinaryDense(128, H='Glorot', kernel_lr_multiplier='Glorot', use_bias=False, name='fc1', input_shape=(16,)))
+    model.add(BinaryDense(128, H=1, kernel_lr_multiplier='Glorot', use_bias=False, name='fc1', input_shape=(16,)))
     model.add(BatchNormalization(epsilon=1e-6, momentum=0.9, name='bn1'))
     model.add(Activation('relu', name='relu{}'.format(1)))
 
-    model.add(BinaryDense(64, H='Glorot', kernel_lr_multiplier='Glorot', use_bias=False, name='fc2'))
+    model.add(BinaryDense(64, H=1, kernel_lr_multiplier='Glorot', use_bias=False, name='fc2'))
     model.add(BatchNormalization(epsilon=1e-6, momentum=0.9, name='bn2'))
     model.add(Activation('relu', name='relu{}'.format(2)))
 
-    model.add(BinaryDense(64, H='Glorot', kernel_lr_multiplier='Glorot', use_bias=False, name='fc3'))
+    model.add(BinaryDense(64, H=1, kernel_lr_multiplier='Glorot', use_bias=False, name='fc3'))
     model.add(BatchNormalization(epsilon=1e-6, momentum=0.9, name='bn3'))
     model.add(Activation('relu', name='relu{}'.format(3)))
 
-    model.add(BinaryDense(nclasses, H='Glorot', kernel_lr_multiplier='Glorot', use_bias=False, name='output'))
+    model.add(BinaryDense(nclasses, H=1, kernel_lr_multiplier='Glorot', use_bias=False, name='output'))
     model.add(BatchNormalization(epsilon=1e-6, momentum=0.9, name='bn'))
 
     return model
